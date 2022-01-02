3aa73e6b098831ee65832681bd803c5abf10788e
Djordje Pejcic <djodjo1234@gmail.com>
2022-01-02 18:08:15 +0100

    All done.

--- src/pastaBar/PastaBar.java
+++ src/pastaBar/PastaBar.java
@@ -1,8 +1,12 @@
 package pastaBar;
 
+import java.util.Scanner;
+
 public class PastaBar {
 
 	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
 
 		String[] ingredients = { "makarone", "spagete", "bolognese", "curetina", "govedja prsuta", "slanina",
 				"piletina", "4 sira", "dimljeni sir", "parmezan", "pavlaka", "pesto sos", "napolitana", "povrce mix",
@@ -12,6 +16,47 @@ public class PastaBar {
 
 		int[] prices = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
 
+		boolean stopOrdering = false;
+		double sumPrices = 0;
+
+		System.out.println("Izvolite!! :-)");
+
+		while (!stopOrdering) {
+
+			System.out.print("Izaberite sastojak za pastu: ");
+			String ingredientName = s.nextLine();
+
+			int ingredientIndex = findIngredient(ingredients, ingredientName);
+
+			if (ingredientName.equalsIgnoreCase("Poruci")) {
+
+				stopOrdering = true;
+
+				System.out.print("Unesite broj telefona: ");
+				String phone = s.next();
+
+				if (isRegularCustomer(phoneNumbers, phone)) {
+					sumPrices = sumPrices * 0.9;
+				}
+
+			} else if (ingredientIndex > -1) {
+
+				sumPrices = sumPrices + prices[ingredientIndex];
+
+			} else {
+				System.out.println("Pogresan unos. Molimo Vas, unesite ispravan naziv sastojaka. :-)");
+			}
+
+		}
+
+		String currency = " dinara.";
+
+		if (sumPrices % 10 == 1) {
+			currency = " dinar.";
+		}
+
+		System.out.println("Cena vase paste iznosi: " + sumPrices + currency);
+		System.out.println("Prijatno!! :-)");
 	}
 
 	public static int findIngredient(String[] ingredients, String ingredientName) {
@@ -20,7 +65,7 @@ public class PastaBar {
 				return i;
 			}
 		}
-		return 0;
+		return -1;
 	}
 
 	public static boolean isRegularCustomer(String[] phoneNumbers, String phone) {
@@ -31,5 +76,4 @@ public class PastaBar {
 		}
 		return false;
 	}
-
 }
